6dd12a0e03211063364bcbe51d3b1f8a4515fafa
AlexWolski <alex.theinfinite@gmail.com>
2020-11-05 19:37:01 -0800

    Set OpenGL version to 3.2

--- openFrameworks/apps/Homework 5 - Ray Tracing/src/main.cpp
+++ openFrameworks/apps/Homework 5 - Ray Tracing/src/main.cpp
@@ -3,8 +3,12 @@
 
 int main()
 {
-	//Set up the GL context
-	ofSetupOpenGL(1024,768,OF_WINDOW);
+	//Create a windowed application running in OpenGL 3.2
+	ofGLFWWindowSettings settings;
+	settings.setGLVersion(3, 2);
+	settings.setSize(1024, 768);
+	ofCreateWindow(settings);
+
 	//Start the application
 	ofRunApp(new ofApp());
 }
