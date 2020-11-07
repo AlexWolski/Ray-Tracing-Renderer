a850e04163284b4c377049f0f8ca3e85caf33aa2
AlexWolski <alex.theinfinite@gmail.com>
2020-11-07 02:51:20 -0800

    Added frame buffer and fov methods to the rtCam class

--- openFrameworks/apps/Homework 5 - Ray Tracing/src/rtGraphics/rtCam.cpp
+++ openFrameworks/apps/Homework 5 - Ray Tracing/src/rtGraphics/rtCam.cpp
@@ -13,7 +13,63 @@ namespace rtGraphics
 		transform.setOrientation(ofQuaternion(rotation));
 	}
 
-	///Scene methods
+	///Destructor
+	rtCam::~rtCam()
+	{
+		deleteFrameBuffer();
+	}
+
+	///Buffer Methods
+	//Creates a 3D image buffer array based on the window size
+	void rtCam::createFrameBuffer()
+	{
+		createFrameBuffer(ofGetWindowWidth(), ofGetWindowHeight());
+	}
+
+	//Creates a 3D image buffer array
+	void rtCam::createFrameBuffer(int width, int height)
+	{
+		//If a frame buffer already exists, delete it
+		if (frameBuffer != NULL)
+			deleteFrameBuffer();
+
+		//Save the dimensions of the buffer
+		bufferWidth = width;
+		bufferHeight = height;
+
+		//Create a triple pointer and allocate an array for the columns of the image
+		char*** array3D = new char**[width];
+
+		//Loop through the columns of the image
+		for (int col = 0; col < width; col++)
+		{
+			//Allocate arrays for each row of the image
+			array3D[col] = new char*[height];
+
+			//Allocate an array for the RGB values. The default color is black
+			for (int row = 0; row < height; row++)
+				array3D[col][row] = new char[3] {0, 0, 0};
+		}
+
+		//Create a shared pointer to the 3D array and save it
+		frameBuffer = make_shared<char***>(array3D);
+	}
+
+	//Deallocate the memory used for the frame buffer
+	void rtCam::deleteFrameBuffer()
+	{
+		for (int col = 0; col < bufferWidth; col++)
+		{
+			//Delete the row arrays
+			for (int row = 0; row < bufferHeight; row++)
+				delete[] frameBuffer.get()[col][row];
+
+			//Delete the column arrays
+			delete[] frameBuffer.get()[col];
+		}
+	}
+
+	///Scene Methods
 	shared_ptr<rtScene> rtCam::getScene()
 	{
 		return scene;
@@ -23,4 +79,21 @@ namespace rtGraphics
 	{
 		this->scene = scene;
 	}
+
+	///Camera Methods
+	void rtCam::setFov(float fov)
+	{
+		this->fov = fov;
+	}
+
+	float rtCam::getFov()
+	{
+		return fov;
+	}
+
+	void rtCam::render()
+	{
+
+	}
+
 }
--- openFrameworks/apps/Homework 5 - Ray Tracing/src/rtGraphics/rtCam.h
+++ openFrameworks/apps/Homework 5 - Ray Tracing/src/rtGraphics/rtCam.h
@@ -12,16 +12,34 @@ namespace rtGraphics
 	class rtCam
 	{
 	private:
+		//The scene to render
 		shared_ptr<rtScene> scene = nullptr;
 		ofNode transform;
 		float fov;
+		//A pointer to a 3D array containing the RGB values for each pixel on the screen
+		shared_ptr<char***> frameBuffer = NULL;
+		//The dimensions of the frame buffer
+		int bufferWidth, bufferHeight;
+
+		///Buffer methods
+		//Instantiates the frame buffer. If no dimensions are given, the window size is used
+		void createFrameBuffer();
+		void createFrameBuffer(int width, int height);
+		//Deallocates the memory of the 3D array
+		void deleteFrameBuffer();
 
 	public:
 		///Constructors
 		rtCam(ofNode transform);
 		rtCam(ofVec3f position, ofVec3f rotation);
+		///Destructor
+		~rtCam();
 		///Scene methods
 		shared_ptr<rtScene> getScene();
 		void setScene(shared_ptr<rtScene> scene);
+		///Camera Methods
+		void setFov(float fov);
+		float getFov();
+		void render();
 	};
 }
