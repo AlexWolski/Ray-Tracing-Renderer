733affd560eb4fb85dcc56235622fb577b9ffff9
AlexWolski <alex.theinfinite@gmail.com>
2020-11-07 20:30:01 -0800

    Created a demo scene in ofApp

--- openFrameworks/apps/Homework 5 - Ray Tracing/src/ofApp.cpp
+++ openFrameworks/apps/Homework 5 - Ray Tracing/src/ofApp.cpp
@@ -2,7 +2,9 @@
 
 void ofApp::setup()
 {
-
+	demoScene = make_shared<rtScene>();
+	mainCamera = make_shared<rtCam>(ofVec3f(0.0f, 0.0f, 0.0f), ofVec3f(90.0f, 0.0f, 0.0f));
+	demoScene->setCamera(mainCamera);
 }
 
 void ofApp::update()
--- openFrameworks/apps/Homework 5 - Ray Tracing/src/ofApp.h
+++ openFrameworks/apps/Homework 5 - Ray Tracing/src/ofApp.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include "ofMain.h"
-#include "./rtGraphics/rtMain.h"
+#include "rtGraphics/rtMain.h"
 
 using namespace rtGraphics;
 
 class ofApp : public ofBaseApp
 {
-	public:
-		void setup();
-		void update();
-		void draw();
+private:
+	shared_ptr<rtScene> demoScene;
+	shared_ptr<rtCam> mainCamera;
+
+public:
+	void setup();
+	void update();
+	void draw();
 };
