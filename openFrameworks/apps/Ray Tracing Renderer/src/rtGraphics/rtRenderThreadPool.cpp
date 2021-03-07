98cbfe8e94cf11c3b37fb36669c72f3b76be08f9
AlexWolski <alex.theinfinite@gmail.com>
2021-03-07 01:46:37 -0800

    Created a RenderThreadPool class

---
+++ openFrameworks/apps/Ray Tracing Renderer/src/rtGraphics/rtRenderThreadPool.cpp
@@ -0,0 +1,140 @@
+#include "rtRenderThreadPool.h"
+
+namespace rtGraphics
+{
+	RenderThreadData::RenderThreadData(shared_ptr<rtScene>scene, rtVec3f& camPos, float nearClip, float farClip,
+		int maxBounces, ofPixels* bufferPixels, rtVec3f& firstPoint, rtVec3f& hStep, rtVec3f& vStep)
+	{
+		//Scene data
+		this->objects = scene->getObjects();
+		this->lights = scene->getLights();
+		//Camera Data
+		this->camPos = camPos;
+		this->nearClip = nearClip;
+		this->farClip = farClip;
+		this->maxBounces = maxBounces;
+		//Buffer data
+		this->bufferPixels = bufferPixels;
+		this->bufferWidth = bufferPixels->getWidth();
+		this->bufferHeight = bufferPixels->getHeight();
+		//Ray iteration data
+		this->firstPoint = firstPoint;
+		this->hStep = hStep;
+		this->vStep = vStep;
+	}
+
+	//Renders a section of the frame buffer
+	void RenderThread::threadedFunction()
+	{
+		//The current index in the buffer pixels array
+		int bufferIndex = sharedData->bufferWidth * startRow * 3;
+
+		//The first grid point of the current row. This vector stays on the left edge of the near clip plane and moves downwards
+		rtVec3f currRow = sharedData->firstPoint + (sharedData->vStep * startRow);
+		//The current grid point. This vector is in the same row as currRowStart and moves to the right
+		rtVec3f R = currRow;
+		//The direction vector from the camera to the current point
+		rtVec3f D;
+
+		//Iterate over all the grid points
+		for (int row = startRow; row < endRow; row++)
+		{
+			for (int col = 0; col < sharedData->bufferPixels->getWidth(); col++)
+			{
+				//Find new direction vector
+				D = (R - sharedData->camPos).normalize();
+				//Calculate the color of the pixel
+				rtColorf pixelColor = rtRenderer::rayTrace(sharedData->objects, sharedData->lights, sharedData->camPos, D, sharedData->nearClip, sharedData->farClip, 0, sharedData->maxBounces);
+				//Write the color to the pixel buffer
+				(*sharedData->bufferPixels)[bufferIndex++] = (int)(pixelColor.getR() * 255.0f);
+				(*sharedData->bufferPixels)[bufferIndex++] = (int)(pixelColor.getG() * 255.0f);
+				(*sharedData->bufferPixels)[bufferIndex++] = (int)(pixelColor.getB() * 255.0f);
+
+				//Iterate to the next column
+				R += sharedData->hStep;
+			}
+
+			//After a row is completed, move to the next row
+			currRow += sharedData->vStep;
+			R = currRow;
+		}
+	}
+
+	int rtRenderThreadPool::numThreads = 8;
+
+	rtRenderThreadPool::rtRenderThreadPool(shared_ptr<rtScene> scene, rtVec3f& camPos, rtVec3f& u, rtVec3f& v, rtVec3f& n,
+										   float hFov, float nearClip, float farClip, int maxBounces, ofPixels* bufferPixels)
+	{
+		//Cache the pixel buffer dimensions as floats
+		float bufferWidth = bufferPixels->getWidth();
+		float bufferHeight = bufferPixels->getHeight();
+
+		//The width and height of the near clip plane based on the FOV and distance to the clip plane
+		float halfClipWidth = sin(degToRad(hFov / 2)) * nearClip;
+		float halfClipHeight = halfClipWidth * (bufferHeight / bufferWidth);
+
+		//Calculate the point in the center of the near clip plane using the camera position and look vector
+		rtVec3f clipCenter = camPos + (-n * nearClip);
+		//Get the vectors pointing from the center of the screen to the left edge and top edge of the near clip plane
+		rtVec3f widthVector = u * halfClipWidth;
+		rtVec3f heightVector = v * halfClipHeight;
+		//The distance between each grid point of the near clip plane in world space
+		rtVec3f hStep = (widthVector * -2) / bufferWidth;
+		rtVec3f vStep = (heightVector * -2) / bufferHeight;
+
+		//The first grid point, at the top-left corner
+		rtVec3f firstPoint = clipCenter + widthVector + heightVector;
+
+		//Scene data
+		sharedData = make_shared<RenderThreadData>(scene, camPos, nearClip, farClip, maxBounces, bufferPixels, firstPoint, hStep, vStep);
+
+		//The minimum number of rows each thread will render
+		int baseRows = bufferHeight / numThreads;
+		//The number of threads that will render an additional row
+		int extraRows = bufferHeight - (numThreads * baseRows);
+		//The starting row of the next section
+		int sectionStart = 0;
+
+		//Spawn threads to render all of the image
+		for (int threadIndex = 0; threadIndex < numThreads; threadIndex++)
+		{
+			//Calculate the end of the section
+			int sectionEnd = sectionStart + baseRows;
+
+			//Add an extra row until all the extra rows are allocated
+			if (threadIndex < extraRows)
+				sectionEnd++;
+
+			//Set the section to render and start the thread
+			threadPool[threadIndex].setSection(sectionStart, sectionEnd);
+			threadPool[threadIndex].startThread();
+
+			//Move onto the next section
+			sectionStart = sectionEnd;
+		}
+
+		//Join all the threads
+		joinThreads();
+	}
+
+	void rtRenderThreadPool::startThreads()
+	{
+		for (int threadIndex = 0; threadIndex < numThreads; threadIndex++)
+			if (!threadPool[threadIndex].isThreadRunning())
+				threadPool[threadIndex].startThread();
+	}
+
+	void rtRenderThreadPool::stopThreads()
+	{
+		for (int threadIndex = 0; threadIndex < numThreads; threadIndex++)
+			if (threadPool[threadIndex].isThreadRunning())
+				threadPool[threadIndex].stopThread();
+	}
+
+	void rtRenderThreadPool::joinThreads()
+	{
+		for (int threadIndex = 0; threadIndex < numThreads; threadIndex++)
+			if (threadPool[threadIndex].isThreadRunning())
+				threadPool[threadIndex].waitForThread();
+	}
+}
---
+++ openFrameworks/apps/Ray Tracing Renderer/src/rtGraphics/rtRenderThreadPool.h
@@ -0,0 +1,75 @@
+#include "memory.h"
+#include "rtRenderer.h"
+
+namespace rtGraphics
+{
+	//A struct containing the data shared by all threads in the pool
+	struct RenderThreadData
+	{
+	public:
+		RenderThreadData(shared_ptr<rtScene>scene, rtVec3f& camPos, float nearClip, float farClip,
+			int maxBounces, ofPixels* bufferPixels, rtVec3f& firstPoint, rtVec3f& hStep, rtVec3f& vStep);
+
+		//Scene data
+		objectSet objects;
+		lightSet lights;
+		//Camera data
+		rtVec3f camPos;
+		float nearClip, farClip;
+		int maxBounces;
+		//Output buffer data
+		ofPixels* bufferPixels;
+		float bufferWidth, bufferHeight;
+		//Grid data
+		rtVec3f firstPoint, hStep, vStep;
+	};
+
+
+	//A threadable nested class that renders a portion of the final image
+	class RenderThread : public ofThread
+	{
+	private:
+		//Shared data
+		shared_ptr<RenderThreadData> sharedData;
+		//Instance data
+		int startRow, endRow;
+
+		//Renders a section of the frame buffer
+		void threadedFunction();
+
+	public:
+		//Set the shared data
+		void setData(shared_ptr<RenderThreadData> sharedData)
+		{
+			this->sharedData = sharedData;
+		}
+
+		//Set the section of the image to render
+		void setSection(int startRow, int endRow)
+		{
+			this->startRow = startRow;
+			this->endRow = endRow;
+		}
+	};
+
+
+	class rtRenderThreadPool
+	{
+	private:
+		//The number of threads in the pool
+		static int numThreads;
+		//Shared thread data
+		shared_ptr<RenderThreadData> sharedData;
+		//A pool of threads to render the image
+		shared_ptr<RenderThread[]> threadPool;
+
+	public:
+		//Initialize a pool of threads given the render settings and scene
+		rtRenderThreadPool(shared_ptr<rtScene> scene, rtVec3f& camPos, rtVec3f& u, rtVec3f& v, rtVec3f& n,
+						   float hFov, float nearClip, float farClip, int maxBounces, ofPixels* bufferPixels);
+		//Thread management methods
+		void startThreads();
+		void stopThreads();
+		void joinThreads();
+	};
+}
