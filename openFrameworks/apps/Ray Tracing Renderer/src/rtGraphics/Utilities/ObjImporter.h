8cacba551c89cf9f315db04c70bfe06873a26e82
AlexWolski <alex.theinfinite@gmail.com>
2021-05-19 03:26:15 -0700

    Added an ObjImporter utility class

---
+++ openFrameworks/apps/Ray Tracing Renderer/src/rtGraphics/Utilities/ObjImporter.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include "../Objects/rtMesh.h"
+
+namespace rtGraphics
+{
+	class ObjImoprter
+	{
+		static rtMesh loadOBJ(string filePath)
+		{
+			return rtMesh();
+		}
+	};
+}
