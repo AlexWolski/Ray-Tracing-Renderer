7a5331030d8a350e9524265a1d44858b783435b0
AlexWolski <alex.theinfinite@gmail.com>
2021-05-19 21:14:29 -0700

    Added a getLongestAxis method for bounding boxes

--- openFrameworks/apps/Ray Tracing Renderer/src/rtGraphics/BVH.h
+++ openFrameworks/apps/Ray Tracing Renderer/src/rtGraphics/BVH.h
@@ -40,8 +40,13 @@ namespace rtGraphics
 
 		//Construct the tree given a list of primitives and their bounding boxes
 		void construct(primitiveList primitives);
+		//A helper function that splits a group of primitives into two children nodes
+		void branchNode(shared_ptr<ObjectNode> subTreeRoot, primitiveList primitives);
+
 		//Creates a bounding box that encapsulates the given primitives
 		rtBoundingBox encapsulae(primitiveList primitives);
+		//Find on which axis the bounding box is longest
+		int getLongestAxis(rtBoundingBox boundingBox);
 	};
 
 
@@ -76,14 +81,22 @@ namespace rtGraphics
 		}
 	}
 
+	//A helper function that splits a group of primitives into two children nodes
+	template <class T>
+	inline void BVH<T>::branchNode(shared_ptr<ObjectNode> subTreeRoot, primitiveList primitives)
+	{
+
+	}
+
 	//Creates a bounding box that encapsulates the given primitives
+	//TO-DO: Move to the bounding box class
 	template <class T>
 	inline rtBoundingBox BVH<T>::encapsulae(primitiveList primitives)
 	{
 		rtVec3f min(INFINITY);
 		rtVec3f max(-INFINITY);
 
-		//Loop throuh all objects in the list
+		//Loop through all objects in the list
 		for (primitivePair pair : primitives)
 		{
 			rtBoundingBox boundingBox = pair.first;
@@ -92,12 +105,35 @@ namespace rtGraphics
 			for (int axis = 0; axis < 3; axis++)
 			{
 				if (boundingBox[axis] < min[axis])
-					min[axis = boundingBox[axis];
+					min[axis] = boundingBox[axis];
 				if (boundingBox[axis] > max[axis])
-					max[axis = boundingBox[axis]
+					max[axis] = boundingBox[axis];
 			}
 		}
 
 		return rtBoundingBox(min, max);
+	}
+
+	//Finds the axis in which the given objects are the most spread out
+	template <class T>
+	inline int BVH<T>::getLongestAxis(rtBoundingBox boundingBox)
+	{
+		//Get the dimensions of the bounding box
+		rtVec3f dim = boundingBox.getMax() - boundingBox.getMin();
+
+		int maxAxis = 0;
+		float maxRange = 0.0f;
+
+		//Determine which axis is longest
+		for (int axis = 0; axis < 3; axis++)
+		{
+			if (dim[axis] > maxRange)
+			{
+				maxAxis = axis;
+				maxRange = dim[axis];
+			}
+		}
+
+		return maxRange;
 	}
 }
